6289bad7c16718caa30033ec2ad75b804f091667
BigBoss951 <um7584062@gmail.com>
2024-03-27 08:27:38 +0100

    ajouter la page d'ajout

---
+++ src/pages/pageAjout.jsx
@@ -0,0 +1,11 @@
+import { ListesA } from "@/components/ListesA";
+import Navbar from "@/components/navbar";
+
+export default function pageAjout() {
+    return(
+    <>
+            <Navbar/>
+        <form type="text" placeholder="Ajouter votre titre ici"></form>
+    </>
+    )
+} 
