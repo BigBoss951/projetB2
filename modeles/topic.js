36418793e747a2a39cd8a585e9923e7e211c6b71
BigBoss951 <um7584062@gmail.com>
2024-03-30 09:56:05 +0100

    Schéma DB

---
+++ modeles/topic.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const { Schema } = mongoose;
+
+const topicSchema = new Schema(
+    {
+        selectedCategory: String,
+        typeCuisine: String,
+        nbEtoiles: Number,
+        prixMoyenRestaurant: Number,
+        courantArtistique: String,
+        typeArt: String,
+        gratuitOuPayant: String,
+        prix: Number,
+        typeBar: String,
+        prixMoyen: Number,
+        typeParc: String,
+        publicOuPrive: String,
+        gratuitOuPayantParc: String,
+        prixParc: Number,
+    },
+    {
+        timestamps: true,
+    }
+);
+
+const Topic = mongoose.model("Topic", topicSchema);
+
+export default Topic;
