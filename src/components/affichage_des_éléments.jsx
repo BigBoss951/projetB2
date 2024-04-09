6039396c2d80e2eb66d5a89c2d53e77da702cbd1
BigBoss951 <um7584062@gmail.com>
2024-04-09 11:22:23 +0200

    deuxieme test pour le bouton delete

--- src/components/affichage_des_éléments.jsx
+++ src/components/affichage_des_éléments.jsx
@@ -1,34 +1,25 @@
 import Link from "next/link";
-import RemoveBtn from "./RemoveBtn";
+import {RemoveBtn} from "./RemoveBtn";
 import { HiPencilAlt } from "react-icons/hi";
 
-export const getServerSideProps = async () => {
-  try {
-    const res = await fetch('http://localhost:3000/api/places');
-
-    if (!res.ok) {
-      throw new Error('Impossible de fetch les sujets')
-    }
-    console.log(res.status)
-    const json = await res.json();
-    console.log(json)
-    return {
-      props: {
-      json
-    }}
-  } catch (error) {
-    return {
-      props: {
-      json: error
-    }}
-  }
-}
-
-export default function ListesA({json}) {
-  console.log(json)
+export default function ListesA({ places }) {
   return (
     <>
-      
+      {places.map((t) => (
+        <div className="p-4 border border-slate-300 my-3 flex justify-between gap-5 items-start" key={t._id}>
+          <div>
+            <h2 className="font-bold text-2xl">{t.selectedCategory}</h2>
+            <div>{t.typeCuisine}</div>
+          </div>
+          <div className="flex gap-2">
+            <RemoveBtn id={t._id}/>
+            <Link href={`/editSujet/${t._id}`}>
+              <HiPencilAlt size={24} />
+            </Link>
+          </div>
+        </div>
+      ))}
     </>
   );
 }
+
